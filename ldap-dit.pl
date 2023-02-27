8efc6acf54dce815b2bf493e88edff844c8b463e
Clément OUDOT <clement@oodo.net>
2023-02-27 07:13:25 +0100

    Add LICENSE and code cleaning

--- ldap-dit.pl
+++ ldap-dit.pl
@@ -1,68 +1,71 @@
 #! /usr/pkg/bin/perl
 
-use strict ;
-use warnings ;
+#====================================================================
+# Script to convert CSV or LDIF into LDIF
+#
+# Copyright (C) Marc Baudoin
+# Copyright (C) LTB-project.org
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# GPL License: http://www.gnu.org/licenses/gpl.txt
+#
+#====================================================================
 
-use Tree::Simple ;	# apt install libtree-simple-perl
+use strict;
+use warnings;
 
-# https://metacpan.org/pod/Tree::Simple
+use Tree::Simple;
 
-# devel/p5-File-Find-Rule-Perl
-# devel/p5-Test-Version
-# devel/p5-Tree-Simple
+my $annuaire = undef;
+my $apex     = undef;
 
-my $annuaire = undef ;
-my $apex = undef ;
-
-while ( <> )
-{
-   if ( my ( $dn ) = /^dn: (.+)$/ )
-   {
-      if ( not defined ( $annuaire ) )
-      {
-         $annuaire->{$dn} = Tree::Simple->new ( $dn ) ;
-         $apex = $dn ;
-      }
-      else
-      {
-         my ( $premier , $suite ) = $dn =~ /^([^,]+),(.+)$/ ;
-         $annuaire->{$dn} = Tree::Simple->new ( $dn , $annuaire->{$suite} ) ;
-      }
-   }
+while (<>) {
+    if ( my ($dn) = /^dn: (.+)$/ ) {
+        if ( not defined($annuaire) ) {
+            $annuaire->{$dn} = Tree::Simple->new($dn);
+            $apex = $dn;
+        }
+        else {
+            my ( $premier, $suite ) = $dn =~ /^([^,]+),(.+)$/;
+            $annuaire->{$dn} = Tree::Simple->new( $dn, $annuaire->{$suite} );
+        }
+    }
 }
 
-my $profondeur = -1 ;
-my @aff_vert = ( ) ;
+my $profondeur = -1;
+my @aff_vert   = ();
 
-print "$apex\n" ;
+print "$apex\n";
 
-$annuaire->{$apex}->traverse
-(
-    sub
-    {
-       my ( $element ) = @_ ;
-       my $tag = $element->getNodeValue ( ) ;
-       $profondeur = $element->getDepth ( ) ;
-       if ( $profondeur != 0 )
-       {
-          foreach my $p ( 0 .. $profondeur -  1 )
-          {
-             if ( $aff_vert[$p] )
-             {
-                print '|   ' ;
-             }
-             else
-             {
-                print '    ' ;
-             }
-          }
-       }
-       print '+-- ' . $tag . "($profondeur)" . "\n" ;
+$annuaire->{$apex}->traverse(
+    sub {
+        my ($element) = @_;
+        my $tag = $element->getNodeValue();
+        $profondeur = $element->getDepth();
+        if ( $profondeur != 0 ) {
+            foreach my $p ( 0 .. $profondeur - 1 ) {
+                if ( $aff_vert[$p] ) {
+                    print '|   ';
+                }
+                else {
+                    print '    ';
+                }
+            }
+        }
+        print '+-- ' . $tag . "($profondeur)" . "\n";
 
-       if ( not $element->isLeaf ( ) )
-       {
-          $aff_vert[$profondeur] = 1 if $element->isFirstChild ( ) ;
-          $aff_vert[$profondeur] = 0 if $element->isLastChild ( ) ;
-       }
-   }
-) ;
+        if ( not $element->isLeaf() ) {
+            $aff_vert[$profondeur] = 1 if $element->isFirstChild();
+            $aff_vert[$profondeur] = 0 if $element->isLastChild();
+        }
+    }
+);
